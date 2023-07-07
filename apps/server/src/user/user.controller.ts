7f647d6ddac80b427ecae75e09cb86572c59940e
pedrofaino <pedrofaino99@gmail.com>
2023-07-07 17:03:20 -0300

    feat: :mag: search-pagination added
    se agrego el feature de search/paginado para artistas y canciones

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -8,13 +8,22 @@ import {
   Delete,
   Put,
   Param,
+  Query,
+  Search,
 } from '@nestjs/common';
 import { UpdateUserDto } from 'src/dto/update-user.dto';
 import { UserService } from './user.service';
+import { PaginationQueryDto } from 'src/dto/pagination-query.dto';
+import { GetArtirtsFilterDto } from 'src/dto/get-artists-filter.dto';
+import { SongsService } from 'src/songs/songs.service';
+import { serialize } from 'v8';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly songService: SongsService,
+  ) {}
 
   @Get()
   async getAll(@Res() res) {
@@ -22,6 +31,34 @@ export class UserController {
       const users = await this.userService.getAll();
       return res.status(HttpStatus.OK).json(users);
     } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Internal server error' });
+    }
+  }
+
+  @Get('/search')
+  async getAllArtits(
+    @Query() pagination: PaginationQueryDto,
+    @Query() { search }: GetArtirtsFilterDto,
+    @Res() res,
+  ) {
+    try {
+      const users = await this.userService.getAllArtist(pagination);
+      let songs = await this.songService.getAllSongs(pagination);
+      let artistUsers = users.filter((user) => user.artist === true);
+      if (search) {
+        const searchLowerCase = search.toLowerCase();
+        (artistUsers = artistUsers.filter((artist) =>
+          artist.name.toLowerCase().includes(searchLowerCase),
+        )),
+          (songs = songs.filter((song) =>
+            song.name.toLowerCase().includes(searchLowerCase),
+          ));
+      }
+      return res.status(HttpStatus.OK).json({ ...songs, ...artistUsers });
+    } catch (error) {
+      console.log(error);
       return res
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
         .json({ message: 'Internal server error' });
